93bcf0950423d8ec2cb5dd2d40b4ce23936b6190
Iftikhor99 <1987-2005@mail.ru>
2020-11-13 18:14:28 +0500

    new changes 1

---
+++ cmd/app/server.go
@@ -0,0 +1,95 @@
+package app
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/Iftikhor99/pkg/banners"
+)
+
+// Server npegctasnseT coOow normyeckwi CepBep Hawero npunomeHna.
+type Server struct {
+	mux *http.ServeMux
+
+	bannersSvc *banners.Service
+}
+
+// NewServer - OyHKUMA-KOHCTpykTOp pina co3maHna cepsepa.
+func NewServer(mux *http.ServeMux, bannersSvc *banners.Service) *Server {
+
+	return &Server{mux: mux, bannersSvc: bannersSvc}
+
+}
+
+//ServeHTTP for
+func (s *Server) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+
+	s.mux.ServeHTTP(writer, request)
+
+}
+
+// Init wHmunannsupyet cepsep (permctpupyet sce Handler's)
+func (s *Server) Init() {
+	s.mux.HandleFunc("/banners.getAll", s.handleGetAllBanners)
+	s.mux.HandleFunc("/banners.getById", s.handleGetBannerByID)
+	s.mux.HandleFunc("/banners.save", s.handleSaveBanner)
+
+	s.mux.HandleFunc("/banners.removeById", s.handleRemoveByID)
+	s.mux.HandleFunc("/banners.getById/", s.handleGetPostByID)
+
+}
+
+func (s *Server) handleGetPostByID(writer http.ResponseWriter, request *http.Request) {
+
+	idParam := request.URL.Query().Get("id")
+
+	id, err := strconv.ParseInt(idParam, 10, 64)
+
+	if err != nil {
+		log.Print(err)
+		http.Error(writer, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	item, err := s.bannersSvc.ByID(request.Context(), id)
+
+	if err != nil {
+		log.Print(err)
+		http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	data, err := json.Marshal(item)
+
+	if err != nil {
+		log.Print(err)
+		http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	_, err = writer.Write(data)
+
+	if err != nil {
+		log.Print(err)
+	}
+
+}
+
+func (s *Server) handleRemoveByID(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func (s *Server) handleSaveBanner(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func (s *Server) handleGetBannerByID(writer http.ResponseWriter, request *http.Request) {
+
+}
+
+func (s *Server) handleGetAllBanners(writer http.ResponseWriter, request *http.Request) {
+
+}
---
+++ pkg/banners/service.go
@@ -0,0 +1,61 @@
+package banners
+
+import (
+	"context"
+	"errors"
+	"sync"
+)
+
+// Service npenctasnset co6oi cepsuc no ynpasnenwo OaHHepamn.
+type Service struct {
+	mu sync.RWMutex
+
+	items []*Banner
+}
+
+// NewService co3qaét cepsuc.
+func NewService() *Service {
+	return &Service{items: make([]*Banner, 0)}
+
+}
+
+// Banner npenctasnaet codoi GaHHep.
+type Banner struct {
+	ID int64
+
+	Title string
+
+	Content string
+
+	Button string
+
+	Link string
+}
+
+// ByID Bo3BpawaeT OaHHep no upeHTHOuKaTopy.
+func (s *Service) ByID(ctx context.Context, id int64) (*Banner, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	for _, banner := range s.items {
+		if banner.ID == id {
+			return banner, nil
+		}
+	}
+
+	return nil, errors.New("item not found")
+}
+
+// All for
+func (s *Service) All(ctx context.Context) ([]*Banner, error) {
+	panic("not implemented")
+}
+
+// Save for
+func (s *Service) Save(ctx context.Context, item *Banner) (*Banner, error) {
+	panic("not implemented")
+}
+
+// RemoveByID for
+func (s *Service) RemoveByID(ctx context.Context, id int64) (*Banner, error) {
+	panic("not implemented")
+}
